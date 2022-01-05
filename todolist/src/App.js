8171c1b21b03603940dd4af690f2e491871f2f32
ganshika <ganshika@bridgetree.com>
2022-01-05 16:01:26 +0530

    Reactlist

---
+++ todolist/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Todo from './component/TodoList/todo'
+//import Testtodo from './component/TodoList/testtodo'
+
+const App = () => {
+  return (
+    <>
+     <Todo/>
+    
+    </>
+  )
+}
+
+export default App
